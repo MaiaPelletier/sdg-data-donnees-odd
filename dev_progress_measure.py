442a4021dbc196d7b8d4ccfb1b156556a62f8577
MaiaPelletier <maia.pelletier15@gmail.com>
2022-01-26 12:29:52 -0500

    update code

--- dev_progress_measure.py
+++ dev_progress_measure.py
@@ -8,24 +8,79 @@ import yaml
 def read_indicator_data(indicator):
 
     data = pd.read_csv("data/indicator_" + indicator + ".csv")
+    print('data is read')
 
-    return(data)
+    return data
 
 
-# Read indicator configuration file here
+# Read indicator configuration file and get the configs needed for measurement
 def read_indicator_config(indicator):
 
     ind_config_path = 'indicator-config/' + indicator + '.yml'
 
     with open(ind_config_path, 'r') as stream:
         config = yaml.safe_load(stream)
+        print('config is read')
 
-    return(config)
+    defaults = {'base_year': 2015, 'target_year': 2030, 'direction': 'negative'}  # set defaults
+    config = {key: value for key, value in config.items() if config[key]}  # remove empty/non-configured keys
+
+    # Loop over the defaults & check if they should be changed based on configs
+    # TODO: I feel like this should use the update() method but i can't figure it out
+    for key in defaults.keys():
+        if key not in config.keys():
+            config[key] = defaults[key]
+
+    # get target from configs, if it exists; or set as blank if it doesn't exist
+    if 'target' in config.keys():
+        config['target'] = float(config['target'])
+    else:
+        config['target'] = ''
+
+    # if target is 0, set to 0.001
+    if config['target'] == 0:
+        config['target'] = 0.001
+    print("target is " + str(config['target']))
+
+    return config
 
 
-def methodology_1(data, direction, t, t_0, x=0.01, y=0.0, z=-0.01):
+def progress_threshold_configs(config, method):
+
+    progress_thresholds = {}
+
+    if 'progress_thresholds' in config.keys():
+        if bool(config['progress_thresholds']):
+            print('updating progress thresholds to configs')
+            progress_thresholds = config['progress_thresholds']
+            progress_thresholds = {key: value for x in progress_thresholds for key, value in x.items()}
+
+    elif method == 1:
+        print('updating progress thresholds to defaults for metho 1')
+        progress_thresholds = {'high': 0.01, 'med': 0, 'low': -0.01}
+
+    elif method == 2:
+        print('updating progress thresholds to defaults for metho 2')
+        progress_thresholds = {'high': 0.95, 'med': 0.6, 'low': 0}
+
+    else:
+        print("error with progress thresholds")
+
+    print(progress_thresholds)
+    config.update(progress_thresholds)
+
+    return config
+
+
+def methodology_1(data, config):
     print("Running methodology 1")
 
+    direction = str(config['direction'])
+    t         = float(config['current_year'])
+    t_0       = float(config['base_year'])
+    x         = float(config['high'])
+    y         = float(config['med'])
+    z         = float(config['low'])
 
     current_value = data.Value[data.Year == t].values[0]
     print('current value is ' + str(current_value))
@@ -54,8 +109,18 @@ def methodology_1(data, direction, t, t_0, x=0.01, y=0.0, z=-0.01):
         return "Error"
 
 
-def methodology_2(data, direction, target, t_tao, t_0, t, x=0.95, y=0.6, z=0.0):
+def methodology_2(data, config):
     print("Running methodology 2")
+
+    direction = str(config['direction'])
+    t         = float(config['current_year'])
+    t_0       = float(config['base_year'])
+    target    = float(config['target'])
+    t_tao     = float(config['target_year'])
+    x         = float(config['high'])
+    y         = float(config['med'])
+    z         = float(config['low'])
+
 
     current_value = data.Value[data.Year == t].values[0]
     print('current value is ' + str(current_value))
@@ -92,56 +157,20 @@ def methodology_2(data, direction, target, t_tao, t_0, t, x=0.95, y=0.6, z=0.0):
         return "Error"
 
 
-
 def measure_indicator_progress(indicator):
 
-    data = read_indicator_data(indicator)
-    config = read_indicator_config(indicator)
+    data = read_indicator_data(indicator)      # read indicator data
+    config = read_indicator_config(indicator)  # read configations
+    print(config)
 
-    defaults = {'base_year': 2015, 'target_year': 2030, 'direction': 'negative'}  # set defaults
-    config = {key: value for key, value in config.items() if config[key]}         # remove empty/non-configured keys
-
-    # Loop over the defaults & check if they should be changed based on configs
-    for key in defaults.keys():
-        if key not in config.keys():
-            config[key] = defaults[key]
-
-    # set values for base year, target year, and desired direction of progress
-    base_year = config['base_year']
-    target_year = config['target_year']
-    direction = config['direction']
-
-    # get target from configs, if it exists; or set as blank if it doesn't exist
-    if 'target' in config.keys():
-        target = float(config['target'])
-    elif 'graph_target_lines' in config.keys():
-        graph_target = config['graph_target_lines'][0]
-        target = float(graph_target['value'])
-    else:
-        target = ''
-
-    # if target is 0, set to 0.001
-    if target == 0:
-        target = 0.001
-    print("target is " + str(target))
-
-    # TODO: I do *not* like this for the progress thresholds - its ugly and not intuitive
-    # if ('progress_thresholds' in config.keys()) and (len(config['progress_thresholds']) > 0):
-    #     progress_thresholds = {k: v for list_item in config['progress_thresholds'] for (k, v) in list_item.items()}
-    # else:
-    #     progress_thresholds = {'high': 0.95, 'med': 0.6, 'low': 0.0}
-    # print(progress_thresholds)
-
-    # get years from data
-    years = data["Year"]
-
-    # set current year to be MAX(Year)
-    current_year = years.max()
+    years = data["Year"]              # get years from data
+    base_year = config['base_year']   # set base year value
+    current_year = float(years.max()) # set current year to be MAX(Year)
     print("current year is " + str(current_year))
+    config.update({'current_year': current_year})
 
     # check if assigned base year is in data
     # if not, assign min(Year) to be base year
-
     # TODO: why is this throwing a warning all of a sudden?
     if base_year not in years.values:
         base_year = years.min()
@@ -153,7 +182,6 @@ def measure_indicator_progress(indicator):
 
     # TODO: i might need to add a config to specify which data point to calculate progress on
     #   right now we're just assuming the total line but some of the cif indicators don't have totals
-
     cols = data.columns.values
     if len(cols) > 2:
         cols = cols[1:-1]
@@ -162,20 +190,22 @@ def measure_indicator_progress(indicator):
 
     print(data)
 
-    # TODO: i wouldn't mind getting rid of these floats in the function call
-    if target == '':
-        # , x=float(progress_thresholds['high']), y=float(progress_thresholds['med']), z=float(progress_thresholds['low'])
-        output = methodology_1(data=data, direction=direction, t=float(current_year), t_0=float(base_year))
+    # determine which methodology to run
+    if config['target'] == '':
+        config = progress_threshold_configs(config, method=1)
+        output = methodology_1(data=data, config=config)
         print(output)
-    elif isinstance(target, float):
-        output = methodology_2(data=data, direction=direction, target=float(target), t_tao=float(target_year), t_0=float(base_year), t=float(current_year))
+
+    elif isinstance(config['target'], float):
+        config = progress_threshold_configs(config, method=2)
+        output = methodology_2(data=data, config=config)
         print(output)
+
     else:
         print("Error")
 
 
 # TODO: Grab first year if year is in fiscal format (e.g. 3.5.2)
-
 measure_indicator_progress('3-2-1')
 
 
